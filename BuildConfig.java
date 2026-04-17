84aa754f3659abb11078aaa0d50d2f4455ba9fbf
Avinash Sahani <alonevivanbabu@gmail.com>
2026-04-17 12:40:56 +0530

    Delete BuildConfig.java

--- BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-
-/** Regard By Gymkhana Studio **/
-package com.myvivanx.myvivanxcalculator;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
