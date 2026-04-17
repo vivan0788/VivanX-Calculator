7ac7e580cc8d550fd74c0b040d02d98592e56090
Avinash Sahani <alonevivanbabu@gmail.com>
2026-04-17 12:17:51 +0530

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,8 @@
+/** Automatically generated file. DO NOT MODIFY */
+
+/** Regard By Gymkhana Studio **/
+package com.myvivanx.myvivanxcalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
